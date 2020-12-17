57d713c9f83b51741982561337572666049b387b
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-17 17:56:10 +0200

    Created Programm.cs and connected StyleCop

---
+++ MyCollection/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyCollection
+{
+    using System;
+
+    /// <summary>
+    /// Class where Main method is located.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Start point of program.
+        /// </summary>
+        /// <param name="args">standart argument.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
