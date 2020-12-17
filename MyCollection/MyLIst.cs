9209c8b418035daebd3294c7cc5c37433d9a8677
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-17 18:06:20 +0200

    Added class MyList.

---
+++ MyCollection/MyLIst.cs
@@ -0,0 +1,15 @@
+﻿namespace MyCollection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Class with custom collection.
+    /// </summary>
+    /// <typeparam name="T">template type.</typeparam>
+    public class MyLIst<T>
+    {
+        private T[] array;
+    }
+}
